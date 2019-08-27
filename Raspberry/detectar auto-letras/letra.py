5e5ac68405df9318273f9a5a391005d080fa6c74
fmirurr <fcomirandau@gmail.com>
2019-08-26 21:21:51 -0400

    separa palabras que se obtienen de las imagenes para poder obtener letras de la patente

---
+++ Raspberry/detectar auto-letras/letra.py
@@ -0,0 +1,23 @@
+#cuenta espacio de cadena para poder obtener la patente
+def Espacio(cadena):
+    global patente
+    cont = 0 #obtiene cantidad de espacios
+    for i in range(len(cadena)):
+        if cadena[i] == ' ':
+            cont += 1
+    if cont == 2:
+        patente = cadena
+
+def Cadenas(split_word):
+    for i in range(len(split_word)):
+        if len(split_word[i]) == 8:
+            Espacio(split_word[i])
+
+
+word = "VOYAGE\nMarubeni\nLD KY 95\nCHILE\n" #caso ejemplo
+words = word.split('\n', 15) #(caracter para split, cantidad maxima de split)
+patente = "" #obtendra la patente del vehiculo
+
+Cadenas(words)
+
+print(patente)
