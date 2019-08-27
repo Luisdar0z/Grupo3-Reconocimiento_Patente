7b45744e798c1e037822c377e34b1dbf7af92dbf
Paulo Jimenez <p.jimnezcisterna@gmail.com>
2019-08-27 12:45:12 -0400

    Web Version 1.1

--- Web Reconocimiento Patentes/proyecto/patentes/views.py
+++ Web Reconocimiento Patentes/proyecto/patentes/views.py
@@ -34,3 +34,45 @@ class VehiculoDelete(DeleteView):
 	template_name = './vehiculo_confirm_delete.html'
 	success_url = reverse_lazy('patentes')
 	
+class VehiculoSearch(TemplateView):
+	model = Vehiculo
+	template_name = './vehiculo_search.html'
+	fields = ['nombrePersona', 'patente', 'estacionamiento', 'deptoAsociado', 'estado', 'rut', 'email', 'telefono']
+	
+class VehiculoSearchView(LoginRequiredMixin, TemplateView):
+	def post(self, request, **kwargs):
+		nombrePersona=''
+		patente=''
+		estacionamiento=''
+		deptoAsociado=''
+		rut=''
+		if "nombrePersona" in request.POST.keys():
+			nombrePersona = request.POST["nombrePersona"]
+		if "patente" in request.POST.keys():
+			patente = request.POST["patente"]
+		if "estacionamiento" in request.POST.keys():
+			estacionamiento = request.POST["estacionamiento"]
+		if "deptoAsociado" in request.POST.keys():
+			deptoAsociado = request.POST["deptoAsociado"]
+		if "rut" in request.POST.keys():
+			rut = request.POST["rut"]
+		
+		vehiculosResultados = []
+		
+		if (nombrePersona != ''):
+			resultado = Vehiculo.vehiculos.get(nombrePersona=nombrePersona)
+			vehiculosResultados.append(resultado)
+		elif (patente != ''):
+			resultado = Vehiculo.vehiculos.get(patente=patente)
+			vehiculosResultados.append(resultado)
+		elif (estacionamiento != ''):
+			resultado = Vehiculo.vehiculos.get(estacionamiento=estacionamiento)
+			vehiculosResultados.append(resultado)
+		elif (deptoAsociado != ''):
+			resultado = Vehiculo.vehiculos.get(deptoAsociado=deptoAsociado)
+			vehiculosResultados.append(resultado)
+		elif (rut != ''):
+			print(Vehiculo.vehiculos.get(rut=rut))
+			resultado = Vehiculo.vehiculos.get(rut=rut)
+			vehiculosResultados.append(resultado)
+		return render(request, 'vehiculos_results.html', {'vehiculos': vehiculosResultados})
