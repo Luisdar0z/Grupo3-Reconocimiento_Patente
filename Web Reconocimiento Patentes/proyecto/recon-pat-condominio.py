1af8cc7be0656a0660f8ce8756dcc5cf781ac9be
Paulo Jimenez <p.jimnezcisterna@gmail.com>
2019-09-12 12:19:45 -0300

    Actualizacion Carpetas

--- Web Reconocimiento Patentes/proyecto/recon-pat-condominio.py
+++ Web Reconocimiento Patentes/proyecto/recon-pat-condominio.py
@@ -8,6 +8,6 @@ def Recon(text_):
 	myquery = ("SELECT nombrePersona, telefono, patente FROM patentes_vehiculo WHERE patente =" + text_)
 	c.execute(myquery)
 	templist=list(c.fetchall())
-	print (templist[0][0])
+	print (templist[0][0] + "\nNumero telefonico: " + templist[0][1] + "\nPatente: " + templist[0][2])
 
 Recon(patente_rec)
