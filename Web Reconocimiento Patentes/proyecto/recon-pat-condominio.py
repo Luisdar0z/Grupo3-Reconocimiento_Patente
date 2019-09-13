c277640399557762e1266b8c8883b127ac24e8ca
Carlos Tobar Olivo <chuck.aileron.sxr@gmail.com>
2019-09-13 12:42:38 -0300

    Query SQL corregida

--- Web Reconocimiento Patentes/proyecto/recon-pat-condominio.py
+++ Web Reconocimiento Patentes/proyecto/recon-pat-condominio.py
@@ -5,7 +5,7 @@ patente_rec ="\"ASDF-34\""
 def Recon(text_):
 	conn = sqlite3.connect("db.sqlite3")
 	c = conn.cursor()
-	query = ("SELECT api_registrovehiculos.id FROM (api_registrovehiculos inner join patentes_vehiculo on api_registrovehiculos.vehiculo = patentes_vehiculo.id) WHERE patentes_vehiculo.patente = "+ text_)
+	query = ("SELECT id FROM patentes_vehiculo WHERE patentes_vehiculo.patente = "+ text_)
 	c.execute(query)
 	templist=list(c.fetchall())
 	print (templist[0][0])
