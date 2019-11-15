be9d024ad2a48aac95ee7285d373dabf5611a371
fmirurr <fcomirandau@gmail.com>
2019-11-14 22:09:06 -0300

    actualizacion comandos API desde raspberry

--- Raspberry/Comandos API/API-Post.py
+++ Raspberry/Comandos API/API-Post.py
@@ -2,48 +2,71 @@ import time
 import sqlite3
 from comandos import *
 
+# Funcion para verificar si la una lista o matriz esta vacia
 def is_empty(data_estructure):
-    if data_estructure:
-        return False
-    else:
-        return True
+	if data_estructure:
+		return False
+	else:
+		return True
 
+# Funcion para obtener el ID del vehiculo reconocido
 def Recon(text_):
-    conn = sqlite3.connect("/home/francisco/Documentos/Grupo3-Reconocimiento_Patente/Web Reconocimiento Patentes/proyecto/db.sqlite3")
-    c = conn.cursor()
-    query = ("SELECT id FROM patentes_vehiculo WHERE patentes_vehiculo.patente = "+ text_)
-    c.execute(query)
-    templist=list(c.fetchall())
-    #tiene datos
-    if is_empty(templist) is False:
-        query2 = ("SELECT tipo FROM patentes_vehiculo WHERE patentes_vehiculo.patente = "+ text_)
-        c.execute(query2)
-        templist2 = list(c.fetchall())
-        if (templist2[0][0] == 'Residente') or (templist2[0][0] == 'Visita') or (templist2[0][0] == 'Servicios'):
-            return templist[0][0]
-    #esta vacio
-    else:
-        return -1
+	conn = sqlite3.connect("../../Web Reconocimiento Patentes/proyecto/db.sqlite3")
+	c = conn.cursor()
+	query = ("SELECT id FROM patentes_vehiculo WHERE patente = " + "\"" + text_ + "\"")
+	c.execute(query)
+	templist = list(c.fetchall())
+	
+	#Si encontro vehiculo registrado
+	if is_empty(templist) is False:
+		query2 = ("SELECT tipo FROM patentes_vehiculo WHERE patente = " + "\"" + text_ + "\"")
+		c.execute(query2)
+		templist2 = list(c.fetchall())
+		if (templist2[0][0] == 'Residente') or (templist2[0][0] == 'Visita') or (templist2[0][0] == 'Servicios'):
+			return templist[0][0]
+	#Si no encontro vehiculo registrado
+	else:
+		return -1
 
+# Funcion para verificar si el vehiculo ya habia entrado y, por ende, definir que esta saliendo
+def Entro(_id):
+	conn = sqlite3.connect("../../Web Reconocimiento Patentes/proyecto/db.sqlite3")
+	c = conn.cursor()
+	query = "SELECT entrada FROM api_registrovehiculos WHERE vehiculo = "+ str(_id) +" ORDER BY api_registrovehiculos.id DESC LIMIT 1;"
+	c.execute(query)
+	templist = list(c.fetchall())
+	
+	if is_empty(templist) is False:
+		#Si el vehiculo ya habia ingresado
+		if (templist[0][0] == 1):
+			return 1
+		#Si no ha ingresado
+		else:
+			return 0
+	else:
+		return 0
 
-def Conocido(_id):
-    hora = time.strftime("%H:%M")
-    # print(hora)
-    dia = time.strftime("%d")
-    mes = time.strftime("%m")
-    anio = time.strftime("%y")
-    # print(dia)
-    # print(mes)
-    # print(anio)
+# Funcion para postar un nuevo registro en la API
+def Post_Vehiculo_Reconocido(_id, _registrado):
+	hora = time.strftime("%H:%M")
+	fecha = time.strftime("%Y-%m-%d")
+	
+	#Agregar(idvehiculo, hora, fecha, entrada, salida, visto, registrado)
+	if (_registrado == 1):
+		if (Entro(_id) == 1):
+			Agregar(_id, hora, fecha, 0, 1, 0, _registrado)
+		else:
+			Agregar(_id, hora, fecha, 1, 0, 0, _registrado)
+	else:
+		Agregar(_id, hora, fecha, 1, 0, 0, _registrado)
 
-    Agregar(id, hora, anio, mes, dia)
-
-
-
-patente_rec ="\"ASDF-34\""
+patente_rec = "ASDF-35"
 id = Recon(patente_rec)
-print(id)
-if id>0:
-    Conocido(id)
+#print(id)
+
+if id > 0:
+	print("Vehiculo Registrado")
+	Post_Vehiculo_Reconocido(id, 1)
 else:
-    print("Vehiculo no conocido")
+	print("Vehiculo no conocido")
+	Post_Vehiculo_Reconocido(patente_rec, 0)
--- Raspberry/Comandos API/comandos.py
+++ Raspberry/Comandos API/comandos.py
@@ -2,30 +2,33 @@ import requests
 import json
 
 #Agregar Datos
-def Agregar(_id, _hora, _day, _month, _year):
-    print("imprimiendo desde comandos: ")
-    print(_id)
-    print(_hora)
-    print(_year)
-    print(_month)
-    print(_day)
+#Agregar(idvehiculo, hora, fecha, entrada, salida, visto, registrado, patente)
+def Agregar(_id, _hora, _fecha, _entrada, _salida, _visto, _registrado):
+	#print("imprimiendo desde comandos: ")
+	#print(_id)
+	#print(_hora)
+	#print(_fecha)
+	#print(_visto)
+	
+	url = "http://localhost:8000/apiregistro/"
 
+	_headers = {
+		"Content-Type": "application/json",
+		"Authorization": "Token 01c8574a530b397e9b20108271b82d3bf8c18906"
+	}
 
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token 01c8574a530b397e9b20108271b82d3bf8c18906',
+	data = {
+		"vehiculo": _id,
+		"hora": _hora,
+		"fecha": _fecha,
+		"entrada": _entrada,
+		"salida": _salida,
+		"visto": _visto,
+		"registrado": _registrado
     }
 
-    #data = "vehiculo=",_id , "hora="+_hora + "day=",_day, "month=",_month, "year="+_year   
+	data_convert = json.dumps(data)
+	print(data_convert)
 
-    data = {
-        "vehiculo": _id,
-        "hora": _hora,
-        "day": _year,
-        "month": _month,
-        "year": _day
-    }
-    
-    data_convert = json.dumps(data)
-
-    response = requests.post('http://localhost:8000/apiregistro/', headers=headers, data=data_convert)
+	response = requests.post(url, data=data_convert, headers=_headers)
+	print(response.text)
